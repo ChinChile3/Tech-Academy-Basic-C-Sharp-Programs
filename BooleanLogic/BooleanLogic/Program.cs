7feef1a581b4cd477b830397541087a7b1907290
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-07-26 16:24:26 -0600

    Boolean Logic Assignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            // the '^' symbol compare two things, and result in true if one of the things is true, BUT NOT BOTH
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            //Will result in false
+            Console.WriteLine(true && true && true && false);
+
+            //Will result in true
+            Console.WriteLine(true && true && true || false);
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Answer these questions to see if you qualify for car insurance.");
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI? Answer \'true\' or \'false\':");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+
+            Console.WriteLine("Qualified");
+            Console.WriteLine(Convert.ToInt32(age) > 15 && Convert.ToBoolean(DUI) == false && Convert.ToInt32(tickets) <= 3);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
