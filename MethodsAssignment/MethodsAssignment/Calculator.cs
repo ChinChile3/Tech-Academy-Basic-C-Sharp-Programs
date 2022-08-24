80ab3a32046d17443efd914b537e91944a0f1b42
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-08-23 20:40:46 -0600

    Methods Assignemnt

---
+++ MethodsAssignment/MethodsAssignment/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Calculator
+    {
+        public int Add(int input)
+        {
+            return input + 10;
+        }
+
+        public int Minus(int input)
+        {
+            return input - 10;
+        }
+
+        public int Multiply(int input)
+        {
+            return input * 10;
+        }
+
+        public int Number { get; set; }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's do some math using the number 10. \nPlease enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calObject = new Calculator();
+
+            int calAddResult = calObject.Add(userInput);
+            Console.WriteLine(userInput + " plus " + 10 + " equals: " + calAddResult);
+
+            int calMinusResult = calObject.Minus(userInput);
+            Console.WriteLine(userInput + " minus " + 10 + " equals: " + calMinusResult);
+
+            int calMultiplyResult = calObject.Multiply(userInput);
+            Console.WriteLine(userInput + " multiplied by " + 10 + " equals: " + calMultiplyResult);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
