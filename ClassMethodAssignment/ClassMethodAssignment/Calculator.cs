4a73b764b0f2797fa4ebdc548b4ce0193cb946a5
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-08-28 23:09:32 -0600

    ClassMethod Assignment

---
+++ ClassMethodAssignment/ClassMethodAssignment/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Calculator
+    {
+        public void Calculate(int input1, out int result)
+        {
+            result = input1 / 2;
+        }
+
+
+        public static void Calculate(int input1, out int addResult, out int minusResult)
+        {
+            addResult = input1 + 10;
+            minusResult = input1 - 10;
+        }
+
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            calculator.Calculate(userInput, out int result);
+            Console.WriteLine(userInput + " divided by 2 equals: " + result);
+
+            Console.WriteLine("Now, please enter another number.");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            Calculator.Calculate(userInput2, out int addResult, out int minusResult);
+
+            Console.WriteLine(userInput2 + " plus " + 10 + " equals: " + addResult);
+            Console.WriteLine(userInput2 + " minus " + 10 + " equals: " + minusResult);
+
+
+            Console.ReadLine();
+        }
+    }
+}
