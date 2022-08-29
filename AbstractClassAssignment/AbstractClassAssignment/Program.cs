45e510eb42988d878187720bdc455baacd461bee
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-08-29 17:44:07 -0600

    AbstractClass Assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { firstName = "Travis", lastName = "Cann" };
+
+            //employee1.firstName = "Travis";
+            //employee1.lastName = "Cann";
+
+            employee1.SayName();
+            Console.ReadLine();
+        }
+    }
+}
