81eec09525e0f747e54640349c38f6c70ec5dfc4
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-08-28 22:05:25 -0600

    Method Assignment using void

---
+++ Method3Assignment/Method3Assignment/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3Assignment
+{
+    class Calculator
+    {
+        public static void Calculate(int input1, int input2)
+        {
+            Console.WriteLine(20 * input1);
+            Console.WriteLine(input2);
+        }
+
+    }
+}
---
+++ Method3Assignment/Method3Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Calculator.Calculate(4, 17);
+            Calculator.Calculate(input1: 23, input2: 33);
+
+
+            Console.ReadLine();
+        }
+    }
+}
