f5069d43c62c464e13ae2a6e8e44bcdc16a3e712
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-08-29 22:50:42 -0600

    Added comments and slight alterations

---
+++ EmployeConsoleApp/EmployeConsoleApp/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeConsoleApp
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        public int ID { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Full Name: " + firstName + " " + lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine(firstName + " " + lastName + " has quit the company.");
+        }
+
+
+        //OVERLOADED OPERATORS
+        public static bool operator ==(Employee<T> employee1, Employee<T> employee2)
+        {
+            return employee1.ID == employee2.ID;
+        }
+        public static bool operator !=(Employee<T> employee1, Employee<T> employee2)
+        {
+            return employee1.ID != employee2.ID;
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
