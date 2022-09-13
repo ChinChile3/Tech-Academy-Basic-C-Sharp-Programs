94279042cb09979054f49e94502bea0d55b674c3
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-09-12 22:12:50 -0600

    Finished TwentyOne game, and Input/Output challenge

---
+++ Input/Input/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userNum = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Aaron\source\logs\InputLog.txt", userNum);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Aaron\source\logs\InputLog.txt"));
+            Console.ReadLine();
+        }
+    }
+}
